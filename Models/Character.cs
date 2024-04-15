6b635342c317be1454339091c3a4bd8fe6ede88f
James Spalding <jamescodebase@outlook.com>
2024-04-15 23:16:11 +0100

    Init

---
+++ Models/Character.cs
@@ -0,0 +1,20 @@
+﻿namespace Character.Models
+{
+    public class Character
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CharacterClass { get; set; }
+        public string CharacterRace { get; set; }
+    }
+
+    public class CharacterAttributes
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constituation { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+    }
+}
