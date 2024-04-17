a421354bd7ae26c0515d5b8da507e5e1b0b65506
James Spalding <jamescodebase@outlook.com>
2024-04-17 23:10:27 +0100

    Added stat summary

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ class DnDSimulator
         Character newCharacter = new Character();
 
         CreateCharacter.CreateNewCharacter(newCharacter);
+
         Console.WriteLine("**************************************************");
         Console.WriteLine("Character successfully created!");
         Console.WriteLine("**************************************************");
@@ -13,5 +14,14 @@ class DnDSimulator
         Console.WriteLine($"Your character's name is {newCharacter.FirstName} {newCharacter.LastName}.");
         Console.WriteLine($"Your character's selected race is {newCharacter.CharacterRace.Race}.");
         Console.WriteLine($"Your character's selected class is {newCharacter.CharacterClass.MainClass}.");
+        Console.WriteLine("**************************************************");
+        Console.WriteLine("Your character's attributes are:");
+        Console.WriteLine($"Strength: {newCharacter.CharacterAttributes.Strength}");
+        Console.WriteLine($"Dexterity: {newCharacter.CharacterAttributes.Dexterity}");
+        Console.WriteLine($"Constitution: {newCharacter.CharacterAttributes.Constitution}");
+        Console.WriteLine($"Intelligence: {newCharacter.CharacterAttributes.Intelligence}");
+        Console.WriteLine($"Wisdom: {newCharacter.CharacterAttributes.Wisdom}");
+        Console.WriteLine($"Charisma: {newCharacter.CharacterAttributes.Charisma}");
+        Console.WriteLine("**************************************************");
     }
 }
